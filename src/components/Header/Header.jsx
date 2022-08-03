5d1a5bd2a2a3baa1875bcdc607568eec3aa39559
miasif <minhazul.islam.asif@gmail.com>
2022-08-03 10:52:34 +0600

    header done

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/images/Netflix-Logo.wine.png';
+import './Header.scss';
+import { ImSearch } from 'react-icons/im';
+
+function Header() {
+  return (
+    <nav className="header">
+      <img src={logo} alt="Netflix Logo" />
+
+      <div>
+        <Link to="/tvshows">TV Shows</Link>
+        <Link to="/movies">Movies</Link>
+        <Link to="/recent">Recently Added</Link>
+        <Link to="/myList">My List</Link>
+      </div>
+      <ImSearch />
+    </nav>
+  );
+}
+
+export default Header;
