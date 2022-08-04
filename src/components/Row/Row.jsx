11b32fc01e8b3d5661270d44a151c7af94c15c0a
miasif <minhazul.islam.asif@gmail.com>
2022-08-04 17:14:44 +0600

    trailer added

--- src/components/Row/Row.jsx
+++ src/components/Row/Row.jsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import axios from '../../axios';
 import './Row.scss';
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
 const baseURL = 'https://image.tmdb.org/t/p/original/';
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState('');
   useEffect(() => {
     async function fatchData() {
       const request = await axios.get(fetchUrl);
@@ -14,14 +17,41 @@ function Row({ title, fetchUrl, isLargeRow }) {
     fatchData();
   }, [fetchUrl]);
   // console.log(movies);
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl('');
+    } else {
+      movieTrailer(movie?.name || '')
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get('v'));
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+
   return (
     <div className="row">
       <h1>{title}</h1>
       <div className="row_posters">
         {movies.map((movie) => (
           <img
-            className={`row_poster ${isLargeRow && 'row_posterLarge'}`}
             key={movie.id}
+            onClick={() => {
+              handleClick(movie);
+            }}
+            className={`row_poster ${isLargeRow && 'row_posterLarge'}`}
             src={`${baseURL}${
               isLargeRow ? movie.poster_path : movie.backdrop_path
             }`}
@@ -29,6 +59,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
           />
         ))}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
